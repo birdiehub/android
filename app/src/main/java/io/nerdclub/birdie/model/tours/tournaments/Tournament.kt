18438becb663e28fd9d9925833c8b23fac7a826a
thibo.verbeerst <verbeerst.thibo@icloud.com>
2023-08-05 15:59:04 +0200

    added Tournament model

--- app/src/main/java/io/nerdclub/birdie/model/tours/tournaments/Tournament.kt
+++ app/src/main/java/io/nerdclub/birdie/model/tours/tournaments/Tournament.kt
@@ -1,3 +1,16 @@
 package io.nerdclub.birdie.model.tours.tournaments
 
-data class Tournament()
+import io.nerdclub.birdie.model.courses.Course
+import io.nerdclub.birdie.model.tours.Tour
+import java.time.LocalDateTime
+import java.time.Year
+
+data class Tournament(
+    val id: Int,
+    val Tour: Tour,
+    val name: String,
+    val year: Year,
+    val course: Course,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime
+)
